78b67147ca3deb0d82499c2da0eb92478d3bfc90
Prajwalnaik1411 <prajwalnaik1411@gmail.com>
2026-02-15 10:51:17 +0530

    first commit

---
+++ READ.md
@@ -0,0 +1,3 @@
+this project is made up completly from scratch 
+this is my first project that i made and i m going to deploy it on aws beanstalk
+thx for reading
---
+++ application.py
@@ -0,0 +1,35 @@
+from flask import Flask,request,jsonify,render_template
+import pickle
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+
+application =Flask(__name__)
+app=application
+
+#importing ridge and scaler pipython application.pyckle
+ridge_model=pickle.load(open("Models_/ridge.pk1",'rb'))
+standard_scaler_model=pickle.load(open("Models_/scaler.pk1",'rb'))
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+@app.route('/predict_data',methods=['GET','POST'])
+def predict_datapoint():
+    if request.method=="POST":
+            Temperature=float(request.form.get('Temperature'))
+            RH= float(request.form.get('RH'))
+            Ws = float(request.form.get('Ws'))
+            Rain = float(request.form.get('Rain'))
+            FFMC = float(request.form.get('FFMC'))
+            DMC = float(request.form.get('DMC'))
+            ISI = float(request.form.get('ISI'))
+            Classes = float(request.form.get('Classes'))
+            Region = float(request.form.get('Region'))
+            new_data_scaled = standard_scaler_model.transform([[Temperature, RH, Ws, Rain, FFMC, DMC, ISI, Region,Classes]])            
+            result=ridge_model.predict(new_data_scaled)
+            return render_template('home.html',results=result[0])
+    else:
+        return render_template('home.html')
+if __name__=="__main__":
+    app.run(host='0.0.0.0')
